332db7f537c0c9954e5c6de1ded6ee83a8d7fea6
324-wsl <yhwzbyg@163.com>
2024-05-02 20:50:35 +0800

    add json.h

---
+++ json/json.h
@@ -0,0 +1,16 @@
+// 定义了 json 报文中一些头部和对应的值
+#ifndef JSONH
+#define JSONH
+#define FUNC "Function"     // 服务名称
+#define IDENTITY "Identity" // 自己的身份
+#define RET "Ret"           // 注册结果
+#define IP "Ip"             // ip地址
+#define PORT "Port"         // 端口号
+
+#define SERVER "server"
+#define CLIENT "client"
+#define RPC_SERVER "RPC"
+#define SUC "Successfully registered"
+#define Fa "Function not exit"
+#define RER "Function exits" // 重复注册
+#endif
