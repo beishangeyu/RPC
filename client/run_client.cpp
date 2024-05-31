a9f0fdcdd115a7edb207603fa1c2bee5fed0faf0
笔记本wsl <eyu.com>
2024-05-31 23:50:05 +0800

    完善了main函数,但是rpc的好像还没写完

--- client/run_client.cpp
+++ client/run_client.cpp
@@ -1,15 +1,19 @@
 #include "client.h"
-// #include "client.cpp"
+#include "client.cpp"
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    if (argc == 2 && argv[1] == "-h")
+
+    if (argc == 2 && string(argv[1]) == "-h")
     {
-        // TODO: 输出帮助信息, 服务列表
-        return;
+        // 输出帮助信息
+        cout << "\t-l + 注册中心 ip 地址\n\t-p + 注册中心端口号\n";
+        return 0;
     }
-    string rpc_ip = argv[2];
+
+    string rpc_ip = string(argv[2]);
     short rpc_port = stoi(argv[4]);
     Client my_client;
     my_client.client_init(rpc_ip, rpc_port);
--- rpc_server/run_rpc.cpp
+++ rpc_server/run_rpc.cpp
@@ -1,12 +1,24 @@
 #include "rpc_server.h"
 #include "rpc_server.cpp"
 
-int main()
+int main(int argc, char *argv[])
 {
+    // 默认监听ip地址和端口号
     Rpc_server my_rpc;
-
     string ip = "127.0.0.1";
     short port = 8000;
-    my_rpc.rpc_init(ip, port);
+    if (argc == 2 && string(argv[1]) == "-h")
+    {
+        // 输出帮助信息
+        cout << "\t-l + 注册中心 ip 地址\n\t-p + 注册中心端口号\n";
+        return 0;
+    }
+    if (argc == 5)
+    {
+        ip = string(argv[2]);
+        port = stoi(argv[4]);
+    }
+    else if (argc == 3)
+        my_rpc.rpc_init(ip, port);
     my_rpc.rpc_start();
 }
